ef9cbe0b67cb99e8b3dfe9b6ec6cec27e2597458
Mason Loring Bliss <mason@blisses.org>
2023-04-20 16:15:54 -0400

    handle some erroneous input

--- sugar.c
+++ sugar.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void usage(void)
+{
+        fputs("Please supply a number:\n"
+            "   if < 40, we convert mmol/l to mg/dl\n"
+            "   if > 40, we convert mg/dl to mmol/l\n", stderr);
+}
+
 void main(int argc, char *argv[])
 {
     float sugar;
 
+    if (argc < 2) {
+        usage();
+        exit(1);
+    }
+
     sugar = strtof(argv[1], NULL);
+    if (sugar == 0) {
+        usage();
+        exit(1);
+    }
 
     if (sugar < 40) {
         printf("%0.1f mmol/l = %0.0f mg/dl\n", sugar, sugar * 18);
